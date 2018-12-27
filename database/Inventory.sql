4a833cd559de86fba0c0889ac895061b11dd63c0
positive12 <edisonheramil@gmail.com>
2018-12-27 18:15:46 +0800

    Database ni

--- database/Inventory.sql
+++ database/Inventory.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Oct 05, 2018 at 11:18 PM
--- Server version: 5.7.23-0ubuntu0.18.04.1
+-- Generation Time: Dec 27, 2018 at 02:14 AM
+-- Server version: 5.7.24-0ubuntu0.18.04.1
 -- PHP Version: 7.2.10-0ubuntu0.18.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -52,7 +52,7 @@ CREATE TABLE `rack` (
 --
 
 CREATE TABLE `server` (
-  `s_id` int(12) NOT NULL,
+  `id` int(12) NOT NULL,
   `s_number` varchar(25) DEFAULT NULL,
   `s_name` varchar(233) NOT NULL,
   `s_ipadd` varchar(252) NOT NULL,
@@ -66,16 +66,6 @@ CREATE TABLE `server` (
   `pinnumber` varchar(213) DEFAULT NULL,
   `s_rack` varchar(23) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `server`
---
-
-INSERT INTO `server` (`s_id`, `s_number`, `s_name`, `s_ipadd`, `s_username`, `s_password`, `s_category`, `s_status`, `s_ipadd1`, `s_ipadd2`, `date`, `pinnumber`, `s_rack`) VALUES
-(12, 'Server 16', 'RDCC Database Server', '10.10.1.2', 'It-admin', '12345678', 'RDCC', 'Active', NULL, NULL, '2018-10-04 16:26:48', '', NULL),
-(13, 'Server 16', 'RDCC Server /SSH/VM', '10.10.1.2', 'it-admin', '12345678', 'RDCC', 'Active', NULL, NULL, '2018-10-04 16:36:57', NULL, NULL),
-(14, 'dsadsa', 'RDCC Server /SSH/VMsss', '10.10.1.2', 'it-admin', '12345678', 'RDCC', 'Active', NULL, NULL, '2018-10-04 16:39:52', 'Cd2035', NULL),
-(15, 'Server 16', 'RDCC Server /SSH/VM', '10.4.0.2', 'it-admin', '12345678', 'RDCC', 'Active', NULL, NULL, '2018-10-04 16:41:53', '223409', 'Rack 2');
 
 -- --------------------------------------------------------
 
@@ -94,10 +84,21 @@ CREATE TABLE `servers` (
 --
 
 INSERT INTO `servers` (`id`, `server`, `date`) VALUES
-(1, 'dsadsa', '2018-10-03 16:08:50'),
-(2, 'sada', '2018-10-03 16:12:48'),
-(3, 'sada', '2018-10-03 16:13:56'),
-(4, 'Server 16', '2018-10-04 15:58:00');
+(5, 'Server 1', '2018-10-06 06:33:27'),
+(6, 'Server 2', '2018-10-06 06:33:37'),
+(7, 'Server 3', '2018-10-06 06:33:46'),
+(8, 'Server 4', '2018-10-06 06:33:58'),
+(9, 'Server 5', '2018-10-06 06:34:07'),
+(10, 'Server 6', '2018-10-06 06:34:24'),
+(11, 'Server 7', '2018-10-06 06:34:31'),
+(12, 'Server 8', '2018-10-06 06:34:41'),
+(13, 'Server 9', '2018-10-06 06:34:49'),
+(14, 'Server 10', '2018-10-06 06:34:58'),
+(15, 'Server 14', '2018-10-06 06:35:25'),
+(16, 'Server  15', '2018-10-06 06:35:44'),
+(18, 'dsdsd', '2018-10-12 10:51:12'),
+(19, 'Server 21', '2018-10-13 08:21:05'),
+(20, '', '2018-10-20 18:46:55');
 
 -- --------------------------------------------------------
 
@@ -120,7 +121,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `type`, `email`, `con_passwords`, `date`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 1, 'edison@g.com', 'admin', '2018-09-22 20:56:05');
+(4, 'admin', '21232f297a57a5a743894a0e4a801fc3', 1, 'admin@gmail.com', 'admin', '2018-12-15 05:28:17');
 
 --
 -- Indexes for dumped tables
@@ -142,7 +143,7 @@ ALTER TABLE `rack`
 -- Indexes for table `server`
 --
 ALTER TABLE `server`
-  ADD PRIMARY KEY (`s_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `servers`
@@ -169,17 +170,17 @@ ALTER TABLE `rack`
 -- AUTO_INCREMENT for table `server`
 --
 ALTER TABLE `server`
-  MODIFY `s_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `servers`
 --
 ALTER TABLE `servers`
-  MODIFY `id` int(23) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(23) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
